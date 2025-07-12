73309b6c4f18a1e460118da913e0c48d758a9ea9
unknown <moonadi0@gmail.com>
2025-07-12 16:51:00 -0400

    First commit for realsies

---
+++ main.py
@@ -0,0 +1,22 @@
+# has to take images and stitch them together
+import cv2 as cv
+from PIL import Image
+
+class Image:
+    def __init__(self, height, width, filePath):
+        self.height = height
+        self.width = width
+        self.filePath = filePath
+
+def testEdges(image1, image2): #checks if image 1 and image 2 fit together
+    image
+
+def main():
+    print("Wassup")
+
+
+if __name__ == '__main__':
+    main()
+
+
+#affine based model can be used for specialized devices
