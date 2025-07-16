b30475a64bfc6131c140d011a9091c9190428e01
unknown <moonadi0@gmail.com>
2025-07-16 17:28:29 -0400

    Added things

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 # has to take images and stitch them together
 import cv2 as cv
+from pathlib import Path
 from PIL import Image
 #img = Image.open()
 #status of 1 from stitcher means there wasn't enough overlap
 #ideas: make stitcher
+
 def main():
-    print("Wassup")
-    imgs = [cv.imread("SquigglesLTest.jpg"), cv.imread("SquigglesRTest.jpg")]
+    print("Wassup") #test message to check if the program works
+    current_dir = Path("ImgTest") 
+    imgs = []
+    for item in current_dir.iterdir():
+        imgs.append(cv.imread(item)) #adds every
+
     stitcher = cv.Stitcher.create()
-    (status, stitched) = stitcher.stitch(imgs)
+    (status, stitched) = stitcher.stitch(imgs) #stitches together images
 
     if status == cv.Stitcher_OK:
-        cv.imwrite("TestResult.jpg", stitched)
+        cv.imwrite("TestResults/newTest.jpg", stitched) #downloads stitched together image
     else:
-        print(f"Bruh: {status}")
+        print(f"Bruh: {status}") #prints error message
 
 
 if __name__ == '__main__':
