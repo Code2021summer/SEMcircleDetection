4ac346c2c86e6d4845968db1c05d4d07b58972cc
unknown <moonadi0@gmail.com>
2025-07-22 11:36:07 -0400

    Started a circle calculator

--- main.py
+++ main.py
@@ -2,29 +2,22 @@
 import cv2 as cv
 from pathlib import Path
 from PIL import Image
+import sympy
+import math
 #img = Image.open()
 #status of 1 from stitcher means there wasn't enough overlap
 #ideas: make stitcher
 
+def findCir(x, y):
+    r = math.sqrt(x**2 + y**2)
+    return r #currently only returns hypotenuse
+    
+
 def main():
-    print("Wassup") #test message to check if the program works
-    current_dir = Path("ImgTest") 
-    imgs = []
-    for item in current_dir.iterdir():
-        imgs.append(cv.imread(item)) #adds every
-
-    stitcher = cv.Stitcher.create()
-    (status, stitched) = stitcher.stitch(imgs) #stitches together images
-
-    if status == cv.Stitcher_OK:
-        cv.imwrite("TestResults/newTest.jpg", stitched) #downloads stitched together image
-    else:
-        print(f"Bruh: {status}") #prints error message
+    x = float(input("Enter the first coordinate: "))
+    y = float(input("Enter the second coord: "))
+    
 
 
 if __name__ == '__main__':
     main()
-
-#img = Image.open("SquigglesFull.jpg")
-#img.show()
-#affine based model can be used for specialized devices
